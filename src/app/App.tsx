af0cc37b818b22ff6a7435cc57f8b93d2f824bcd
KristinaWolf <student.pri@yandex.ru>
2022-10-05 23:47:47 +0300

    Внесение fonts и colors, реализация icons и mixins

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { TestComponent } from 'features';
 import React from 'react';
 import { LadIcon } from 'shared';
 
+
 const App = () => (
   <div className="App">
     <TestComponent a={0} />
---
+++ src/shared/ui/icons/HeartIcon.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const HeartIcon = () => (
+<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_54_1837)">
+<path d="M6.77599 6.91602H0V26.2154H6.77599V6.91602Z" fill="#58D9EF"/>
+<path d="M14.0389 0H6.28906V6.91631H14.0389V0Z" fill="#58D9EF"/>
+<path d="M20.8553 6.91602H14.0793V14.8263H20.8553V6.91602Z" fill="#58D9EF"/>
+<path d="M27.6315 14.8672H20.8555V21.7835H27.6315V14.8672Z" fill="#58D9EF"/>
+<path d="M34.4073 7.91016H27.6313V14.8265H34.4073V7.91016Z" fill="#58D9EF"/>
+<path d="M42.1978 1.49023H34.448V8.40654H42.1978V1.49023Z" fill="#58D9EF"/>
+<path d="M41.224 26.2578H34.448V33.6711H41.224V26.2578Z" fill="#58D9EF"/>
+<path d="M34.4075 33.6699H26.6577V40.5862H34.4075V33.6699Z" fill="#58D9EF"/>
+<path d="M26.6576 40.5859H19.8816V47.9992H26.6576V40.5859Z" fill="#58D9EF"/>
+<path d="M19.8817 33.6699H13.1057V40.5862H19.8817V33.6699Z" fill="#58D9EF"/>
+<path d="M13.0651 26.2578H6.28906V33.6711H13.0651V26.2578Z" fill="#58D9EF"/>
+<path d="M48.0001 8.40625H41.2241V26.2147H48.0001V8.40625Z" fill="#58D9EF"/>
+</g>
+<defs>
+<clipPath id="clip0_54_1837">
+<rect width="48" height="48" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+);
