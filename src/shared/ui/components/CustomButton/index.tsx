6c6cb073d75be51c3109e87f3d8a34cbf360c3c7
Irina <irene7744@gmail.com>
2022-10-06 12:22:23 +0300

    refactor: move button styles names obj into constants file

--- src/shared/ui/components/CustomButton/index.tsx
+++ src/shared/ui/components/CustomButton/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'antd';
-import style from './CustomButton.module.scss';
+// import style from './CustomButton.module.scss';
+import { buttonSize, buttonColor } from './constants';
 
 type TProps = {
   children?: string;
@@ -9,25 +10,11 @@ type TProps = {
   customType?: 'default' |'light' | 'transparent' ;
 }
 
-const stylesObj = {
-  size: {
-    large: style.large,
-    medium: style.medium,
-    small: style.small,
-    rectangular: style.rectangular,
-  },
-  color: {
-    default: style.default,
-    light: style.light,
-    transparent: style.transparent,
-  },
-};
-
 export const CustomButton = ({
   children,
   customSize = 'medium',
   customType = 'default',
   ...props
 }: TProps) => (
-  <Button className={`${stylesObj.size[customSize]} ${stylesObj.color[customType]}`} {...props}>{children}</Button>
+  <Button className={`${buttonSize[customSize]} ${buttonColor[customType]}`} {...props}>{children}</Button>
 );
