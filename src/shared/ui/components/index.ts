25720c0c4e517075a06319f75cb3530227b91554
Daniel Dmitrenko <dandmitrenko@gmail.com>
2022-10-27 22:38:19 +0300

    Input field component added. Default, hover, focus, error, disabled styles added. CloseIcon added to icons. Placeholder text color NOT added.

--- src/shared/ui/components/index.ts
+++ src/shared/ui/components/index.ts
@@ -2,3 +2,5 @@ export { Button } from './Button';
 export { Footer } from './Footer';
 export { Header, HeaderSecondaryPage } from './Header';
 export { MenuPopup } from './MenuPopup';
+export { Input } from './Input';
+export { Select } from './Select';
---
+++ src/shared/ui/icons/InputCloseIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const InputCloseIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clipPath="url(#clip0_47_1677)">
+      <path
+        d="M19 6.225L17.775 5L12 10.775L6.225 5L5 6.225L10.775 12L5 17.775L6.225 19L12 13.225L17.775 19L19 17.775L13.225 12L19 6.225Z"
+        fill="black"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_47_1677">
+        <rect width="24" height="24" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
